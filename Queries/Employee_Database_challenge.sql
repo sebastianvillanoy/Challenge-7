c1b52d9d2775bf5e8b05d3074b0bce793e3fab87
Sean Villanoy <seanvillanoy@Seans-MacBook-Pro.local>
2020-12-07 02:11:47 -0500

    Finished Deliverables 1 and 2

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,48 @@
+-- DELIVERABLE 1
+
+--Create retirement_titles table 
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+-- Create unique_titles table by using Distinct ON with Orderby to remove duplicate rows 
+SELECT DISTINCT ON (emp_no) emp_no, 
+	first_name,
+	last_name,
+	title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+-- Create the retiring_titles table
+SELECT COUNT(title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+-- DELIVERABLE 2
+SELECT DISTINCT ON(e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+INNER JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no, t.to_date DESC;
+
